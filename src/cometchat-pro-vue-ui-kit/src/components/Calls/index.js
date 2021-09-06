27caf96d77d86677875b7192e34a752b03e7e4ee
prakash-nakrani <prakash.nakrani@cometchat.com>
2021-09-06 13:46:52 +0530

    v3.0.0-1

--- src/cometchat-pro-vue-ui-kit/src/components/Calls/index.js
+++
@@ -1,2 +0,0 @@
-export { default as CometChatIncomingCall } from "./CometChatIncomingCall/CometChatIncomingCall";
-export { default as CometChatOutgoingCall } from "./CometChatOutgoingCall/CometChatOutgoingCall";
